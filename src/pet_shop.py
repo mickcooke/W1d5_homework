9299388cf5975e680675a4e51d9bad49221a0191
Mick Cooke <mickcooke@Micks-MacBook-Pro-2.local>
2023-05-19 16:12:07 +0100

    completed MVP

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,3 +1,7 @@
+# import pdb
+
+
+
 def get_pet_shop_name(shop):
     name = shop["name"]
     return name
@@ -5,3 +9,66 @@ def get_pet_shop_name(shop):
 def get_total_cash(shop):
     sum = shop["admin"]["total_cash"]
     return sum
+
+def add_or_remove_cash(shop, amount):
+    shop["admin"]["total_cash"] += amount
+
+def get_pets_sold(shop):
+    sold = shop["admin"]["pets_sold"]
+    return sold
+
+def increase_pets_sold(shop, pet_count):
+    shop["admin"]["pets_sold"] += pet_count
+
+def get_stock_count(shop):
+    count = len(shop["pets"])
+    return count
+
+def get_pets_by_breed(shop, breed):
+    pets = []
+    for pet in shop["pets"]:
+        if pet["breed"] == breed:
+            pets.append(pet)
+
+    return pets
+
+def find_pet_by_name(shop, pet_name):
+    for pet in shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+        
+    return None
+
+def remove_pet_by_name(shop, pet_name):
+     for pet in shop["pets"]:
+        if pet["name"] == pet_name:
+            shop["pets"].remove(pet)
+
+def add_pet_to_stock(shop, new_pet):
+    shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    cash = customer["cash"]
+    return cash
+
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    count = len(customer["pets"])
+    return count
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+
+
+
+        
+    
+
+
+
+
+
+
+
